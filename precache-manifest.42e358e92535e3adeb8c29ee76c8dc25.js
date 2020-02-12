e500ebfddcd0b49ef3da8fab88e1f3879f0bbd86
Sergey Burnevsky <sergey.burnevsky@gmail.com>
2020-02-12 23:45:43 +0200

    First demo

---
+++ precache-manifest.42e358e92535e3adeb8c29ee76c8dc25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d808fc842b735050a885aad582d49b",
+    "url": "/Users/sergey/src/workout.js/site/build/index.html"
+  },
+  {
+    "revision": "ccb0a6ef57a56a224f48",
+    "url": "/Users/sergey/src/workout.js/site/build/static/css/main.08d5febe.chunk.css"
+  },
+  {
+    "revision": "ece543019ec1a2ca8161",
+    "url": "/Users/sergey/src/workout.js/site/build/static/js/2.7e5f1f8d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Users/sergey/src/workout.js/site/build/static/js/2.7e5f1f8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccb0a6ef57a56a224f48",
+    "url": "/Users/sergey/src/workout.js/site/build/static/js/main.12731909.chunk.js"
+  },
+  {
+    "revision": "176a89ca70839143c714",
+    "url": "/Users/sergey/src/workout.js/site/build/static/js/runtime-main.7f5393b3.js"
+  }
+]);
